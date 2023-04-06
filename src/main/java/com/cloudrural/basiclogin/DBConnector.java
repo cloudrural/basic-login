7398e9b2c81137df4c068629ca03e0de32f4532c
Bharath Pantala <bpantala@securonix.com>
2023-04-06 12:27:39 +0530

    great files

---
+++ src/main/java/com/cloudrural/basiclogin/DBConnector.java
@@ -0,0 +1,11 @@
+package com.cloudrural.basiclogin;
+
+public class DBConnector {
+    public static void main(String args[]){
+        System.out.println("Hello OOps");
+    }
+
+    public void ordersList(){
+        System.out.println("OrdersList");
+    }
+}
